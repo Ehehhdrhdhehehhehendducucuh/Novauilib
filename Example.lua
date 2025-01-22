37aba854c8d8ad4d3ab6be02ad308f40c5bdc134
Ehehhdrhdhehehhehendducucuh <142185912+Ehehhdrhdhehehhehendducucuh@users.noreply.github.com>
2025-01-22 22:23:42 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local Novauilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Novauilib/refs/heads/main/obf_HFjox9GdMybx3eJV2rV95mbCMfDX0e95Af1sZ2C77BO3AlXqzt249UK6uKqdxI8r.lua"))()
+local Novauilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ehehhdrhdhehehhehendducucuh/Novauilib/refs/heads/main/OpenSourcelib.lua"))()
 
 
 -- examples
